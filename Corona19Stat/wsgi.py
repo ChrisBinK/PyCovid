a6d89c27df3d1fa35c33601ac313ed283a973616
Christelle <christellekalanda@gmail.com>
2020-04-24 21:06:10 +0200

    update whitemoise

--- Corona19Stat/wsgi.py
+++ Corona19Stat/wsgi.py
@@ -10,10 +10,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 
-from whitenoise.django import DjangoWhiteNoise
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Corona19Stat.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
