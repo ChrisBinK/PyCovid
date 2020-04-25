ac3947c9bbc8a51d51dadeb9c4afa3b16af6ca61
Christelle <christellekalanda@gmail.com>
2020-04-25 15:37:10 +0200

    email update

--- Corona19Stat/settings.py
+++ Corona19Stat/settings.py
@@ -131,3 +131,11 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER = 'christellebinkal@gmail.com'
+EMAIL_HOST_PASSWORD = '20192019'
+DEFAULT_FROM_EMAIL = 'christellebinkal@gmail.com'
+EMAIL_USE_TLS = True
